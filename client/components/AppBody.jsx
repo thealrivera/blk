26b3f96e01b5c6463c7f9c7b1576865a2cf3d00c
Al Rivera <thealrivera@gmail.com>
2015-10-04 23:21:16 -0400

    end section 3

--- client/components/AppBody.jsx
+++ client/components/AppBody.jsx
@@ -1,7 +1,48 @@
+let Transition = React.addons.CSSTransitionGroup;
+
 AppBody = React.createClass({
+  getDefaultProps() {
+  return {
+    tabs: ["Tab 1", "Tab 2", "Tab 3"]
+    }
+  },
+  getInitialState() {
+    return {
+      modal: false
+    }
+  },
+  ionModal(tab) {
+    this.setState({
+      modal: (
+        <IonModal>
+          <div className="h1 title">{tab}</div>
+          <button onClick={ () => this.setState({modal:false}) } className="button button-icon active">
+            <i className="icon ion-ios-close-empty"></i>
+          </button>
+        </IonModal>
+      )
+    })
+  },
   render() {
     return (
       <div className="ionic-body">
+      
+        {this.state.modal ? <Backdrop /> : false}
+        <Transition transitionName="modal">
+          {this.state.modal}
+        </Transition>
+
+        <div className="tabs tabs-icon-top">
+          {this.props.tabs.map((tab, i) => {
+              return (
+                <a className="tab-item" key={tab} onClick={this.ionModal.bind(null, tab)}>
+                  <i className="icon ion-star"></i>
+                  {tab}
+                </a>
+              )
+            })
+          }
+        </div>
         <div className="bar bar-header bar-light">
           <ReactRouter.Link className="button button-icon icon ion-gear-a" to={"/settings"}></ReactRouter.Link>
           <ReactRouter.Link className="h1 title" to={"/"}>BLK</ReactRouter.Link>
